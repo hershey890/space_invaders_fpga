728abaf235c52d129e1f24ad83a92842571ef7d0
Hersh Joshi <hersh.joshi1@gmail.com>
2019-11-26 15:30:18 -0800

    clk prototype complete

--- top_clock.v
+++ top_clock.v
@@ -13,4 +13,34 @@ module top_clock(
     output player_move_clk,
 );
 
+    //100 Mhz -> 
+    reg [26:0] block_counter; //run at 0.5 Hz
+    reg [24:0] laser_counter;   //run at 2 Hz
+    reg [24:0] player_move_counter; //run at 2 Hz
+
+    always @ (posedge clk, posedge rst) begin
+        if (rst) begin
+            block_clk <= 0;
+            laser_clk <= 0;
+            player_move_clk <= 0;
+            block_counter <= 27'd100000000;
+            laser_counter <= 25'd25000000;
+            player_move_counter <= 25'd25000000;
+        end
+        else begin
+            if(block_counter == 0) begin
+                block_counter <= 25'd25000000;
+                block_clk <= ~block_clk;
+            end
+            if(laser_counter == 0) begin
+                laser_counter <= 25'd25000000;
+                laser_clk <= ~laser_clk;
+            end
+            if(player_move_counter == 0) begin
+                player_move_counter <= 25'd25000000;
+                player_move_clk <= ~player_move_clk;
+            end
+        end
+    end    
+
 endmodule
